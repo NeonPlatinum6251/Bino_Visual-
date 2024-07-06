9c1d5cf6923d353c4457b4753142cd9f6574cb13
Joseph6251N <josephmarch70@gmail.com>
2024-07-06 23:06:43 +0100

    Update main.py
    I pretty much overhauled the whole thing and finally got the maths to work at a basic level

--- main.py
+++ main.py
@@ -1,39 +1,34 @@
+import turtle
 
-import math
-import turtle #fuck it we are turtling
+sigmasums =float(0.0)
+X= float(0.0)
+P = [0.05, 0.10, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
+N = int(input("Enter the number of trials: "))  # gets the N for the equation
 
-N=int(input("Enter the number of trials: "))          #gets the N for the equation
-def init():
-    X=0   #these are going to be shown on the y-axis, it is the amount of successes for N trials it is not necessary but it one big block of colour at the bottom
-    P=[0.05,0.10,0.15,0.2,0.25,0.3,0.35,0.4,0.45,0.5] #this is going to be the probabilities of success shown on the x-axis might add more later
-    sigmasums=0
-    return X, P, sigmasums
+def howtafindX(N,P):  # this finds how many successes we can have before it is guaranteed calculated by the first time X equals 1 when p is 0.5
+    X=0
+    for j in range(len(P)):
+        if binocd(N,X,P[j])==1:
+            return X
+        X+=1
+    return X
 
-def howtafindX(X): #this finds how many successes we can have before it is guaranteed calculated by the first time X equals 1 when p is 0.5
-    while X!=1:
-        X=binocd(N,X,P)
+# Captial sigma (Σ) applies the expression after it to all members of a range and then sums the results. VERY IMPORTANT NEED TO REMEMBER
 
-
-    return
-
-def binoequation():
-
-
-    return
-
-#Captial sigma (Σ) applies the expression after it to all members of a range and then sums the results. VERY IMPORTANT NEED TO REMEMBER
-def sigma(X,sigmasums): #this is going to be used in the equation for working out the probability
-    for i in range(1,X+1):
-        sigmasums+=const * i
-
+def sigma(X, sigmasums,equation,N,P):  # this is going to be used in the equation for working out the probability
+    for i in range(0, X + 1):
+        sigmasums += equation(N,i,P)
     return sigmasums
 
-def binocd(N,X,P): # doin' tha maff  
-
-    colourstrength=   #I will store the answer of the binomial as the colour strength
-
-    return colourstrength
-
-def colourvalue(colourstrength):
+def binocd(N, X, P):  # doin' tha maff
+      return P ** X * (1 - P) ** (N - X)
 
 
+def equation(N,i,P): #stupid fucking function
+    return binocd(N,i,P[i%len(P)])
+
+result = sigma(int(X),sigmasums,equation,N,P)
+# this is just to test the maths
+X=howtafindX(N,P)
+print(result)
+print(X)
