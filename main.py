29f8c627cead54d9bdd63ebf476610fd8bffb981
Joseph6251N <josephmarch70@gmail.com>
2024-07-06 23:43:29 +0100

    Update main,py
    I've fucked it but too tired to fix

--- main.py
+++ main.py
@@ -5,12 +5,16 @@ X= float(0.0)
 P = [0.05, 0.10, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5]
 N = int(input("Enter the number of trials: "))  # gets the N for the equation
 
+def binocd(N, X, P):  # doin' tha maff
+    return P ** X * (1 - P) ** (N - X)
+
 def howtafindX(N,P):  # this finds how many successes we can have before it is guaranteed calculated by the first time X equals 1 when p is 0.5
-    X=0
-    for j in range(len(P)):
-        if binocd(N,X,P[j])==1:
-            return X
-        X+=1
+    X = 0
+    while True:
+        probab = [binocd(N,X+1,p) for p in P]
+        if any(prob >= 1 for prob in probab):
+            break
+        X += 1
     return X
 
 # Captial sigma (Σ) applies the expression after it to all members of a range and then sums the results. VERY IMPORTANT NEED TO REMEMBER
@@ -20,15 +24,12 @@ def sigma(X, sigmasums,equation,N,P):  # this is going to be used in the equatio
         sigmasums += equation(N,i,P)
     return sigmasums
 
-def binocd(N, X, P):  # doin' tha maff
-      return P ** X * (1 - P) ** (N - X)
-
 
 def equation(N,i,P): #stupid fucking function
     return binocd(N,i,P[i%len(P)])
 
 result = sigma(int(X),sigmasums,equation,N,P)
 # this is just to test the maths
-X=howtafindX(N,P)
+howtafindX(N,P)
 print(result)
 print(X)
